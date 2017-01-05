12ff991369c2137eff17d5275583771cbc2f3054
Emily Monte Calvo <emilymontecalvo@gmail.com>
2017-01-04 16:51:42 -0800

    Update player.rb

--- player.rb
+++ player.rb
@@ -13,15 +13,12 @@ class Player
 
   def get_move(display)
     begin
-      display_msg = "#{@name} pick your piece."
+      display_msg = "#{@name} : pick your piece by using the arrows on your keyboard and hitting enter."
       selected_square = display.get_square(display_msg)
       selected_piece = display.board[*selected_square]
       unless selected_piece.color == @color
         raise ColorError
       end
-
-      #TODO send feedback to board to light up seleted square
-      # and add a variable "e" for the error to puts out
 
       available_moves = selected_piece.get_moves
       display_msg = "Where would you like to move #{selected_piece.class}?"
